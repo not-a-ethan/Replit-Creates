3e1697aa5e42c4cf2c3760d5be89a806b6a2ad3c
Ethan-Master-Coding <92686703+not-a-ethan@users.noreply.github.com>
2022-08-28 15:31:46 -0400

    Create index.js

---
+++ Week 2/day 6/index.js
@@ -0,0 +1,5 @@
+const stuff = ['host', 'program', 'have fun', 'work together']
+
+console.log('Reepliiit')
+console.log(`The one place to ${stuff[Math.floor(Math.random() * stuff.length)]}`)
+console.log('Reeepliiiiiiiiiiiiiiit!')
