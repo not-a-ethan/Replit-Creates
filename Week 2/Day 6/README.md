d60b066feb1c2f5d1883777b7a23308919d8f045
Ethan-Master-Coding <92686703+not-a-ethan@users.noreply.github.com>
2022-08-28 14:49:08 -0400

    Removed some stuff

--- Week 2/Day 6/README.md
+++
@@ -1,3 +0,0 @@
-# August 8 - Write a song
-
-I made a JS program that would just say replit then say that it is the place to {host, program, have fun and, work together} and then say Replit again.
--- Week 2/Day 6/code/index.js
+++
@@ -1,5 +0,0 @@
-const stuff = ['host', 'program', 'have fun', 'work together']
-
-console.log('Reepliiit')
-console.log(`The one place to ${stuff[Math.floor(Math.random() * stuff.length)]}`)
-console.log('Reeepliiiiiiiiiiiiiiit!')
