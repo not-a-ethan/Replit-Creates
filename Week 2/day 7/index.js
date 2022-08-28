ca84a236db879f583fcf90a8cf6c9cfa6fd44e72
Ethan-Master-Coding <92686703+not-a-ethan@users.noreply.github.com>
2022-08-28 15:33:45 -0400

    Create index.js

---
+++ Week 2/day 7/index.js
@@ -0,0 +1,3 @@
+const fortunes = ['you will be rich', 'you will be poor', 'you will be happy', 'you will be sad', 'you will be angry'];
+
+console.log(`You fortune is that ${fortunes[Math.floor(Math.random() * fortunes.length)]}`)
